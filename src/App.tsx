7adf54c58f8ba96bbf0cc8f47e0e9981123ba97d
Damell Bell <58995862+damell01@users.noreply.github.com>
2025-05-14 10:54:44 -0500

    Add Manage Users page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import AdminDashboardPage from './pages/AdminDashboardPage';
 import CustomersPage from './pages/CustomersPage';
 import AnalyticsPage from './pages/AnalyticsPage';
 import AdminBillingPage from './pages/AdminBillingPage';
+import ManageUsersPage from './pages/ManageUsersPage';
 import CustomerDashboardPage from './pages/CustomerDashboardPage';
 import PixelSetupPage from './pages/PixelSetupPage';
 import LegalBestPracticesPage from './pages/LegalBestPracticesPage';
@@ -72,6 +73,15 @@ function App() {
               element={
                 <ProtectedRoute requiredRole="admin">
                   <AdminBillingPage />
+                </ProtectedRoute>
+              } 
+            />
+
+            <Route 
+              path="/admin/users" 
+              element={
+                <ProtectedRoute requiredRole="admin">
+                  <ManageUsersPage />
                 </ProtectedRoute>
               } 
             />
